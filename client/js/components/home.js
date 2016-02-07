77c8abde26e99db1ffc73b15c8da92df1ef06dc2
Daniel Loomer <fuzetsu490@gmail.com>
2016-02-07 16:44:53 -0500

    converted tabs to spaces in home.js

--- client/js/components/home.js
+++ client/js/components/home.js
@@ -1,13 +1,13 @@
 app.cmp.Home = {
-	controller: function(args) {
-		var ctrl = {
-			message: m.prop('This is the home page..')
-		};
-		return ctrl;
-	},
-	view: function(ctrl, args) {
-		return m('div', [
-			m('h2.center', ctrl.message())
-		]);
-	}
+  controller: function(args) {
+    var ctrl = {
+      message: m.prop('This is the home page..')
+    };
+    return ctrl;
+  },
+  view: function(ctrl, args) {
+    return m('div', [
+      m('h2.center', ctrl.message())
+    ]);
+  }
 };
